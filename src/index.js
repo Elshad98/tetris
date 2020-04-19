c488beb32784494282244553c8709af5d98c4953
Elshad98 <safarov.elshad007@gmail.com>
2020-04-19 17:45:08 +0500

    Update webpack.config.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './css/main.css';
+
+
+const arr = [1, 2];
+
+const [one, two] = arr
+
+console.log(one, two);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,49 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const config = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, './build'),
         filename: 'main.js',
+        publicPath: 'build/'
     },
+    devServer: {
+        overlay: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            {
+                test: /\.html$/i,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+        ],
+    },
+    plugins: [
+        new ExtractTextPlugin("main.css"),
+    ]
 };
 
-module.exports = config;
+module.exports = (env, options) => {
+    const production = options.mode === 'production';
+
+    config.devtool = production ? false : 'evel-sourcemap';
+
+    return config;
+};
