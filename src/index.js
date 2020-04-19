450e91c8bc25865bb561a9c715480cee94fef2b1
Elshad98 <safarov.elshad007@gmail.com>
2020-04-19 18:49:15 +0500

    Update webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
+import 'reset-css';
 import './css/main.css';
-
-
-const arr = [1, 2];
-
-const [one, two] = arr
-
-console.log(one, two);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const config = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, './build'),
-        filename: 'main.js',
-        publicPath: 'build/'
+        filename: 'js/main.js',
+        publicPath: ''
     },
     devServer: {
         overlay: true
@@ -19,24 +23,78 @@ const config = {
                 exclude: '/node_modules/'
             },
             {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: "css-loader"
-                })
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader", options: {}
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                autoprefixer({
+                                    overrideBrowserslist: ['last 100 versions'],
+                                    cascade: false
+                                }),
+                            ],
+                        },
+                    },
+                ]
             },
             {
-                test: /\.html$/i,
+                test: /\.html$/,
                 use: [
                     {
                         loader: "html-loader"
                     }
                 ]
             },
+            {
+                test: /\.ico$/,
+                loader: "url-loader",
+                query: { mimetype: "image/x-icon" }
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'img/',
+                        publicPath: 'img/'
+                    }
+                }]
+            },
         ],
     },
     plugins: [
-        new ExtractTextPlugin("main.css"),
+        new MiniCssExtractPlugin({
+            filename: 'css/main.css',
+            chunkFilename: 'css/main.css',
+        }),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: cssnano,
+            cssProcessorPluginOptions: {
+                preset: ['default', {
+                    discardComments: {
+                        removeAll: true
+                    }
+                }],
+            },
+            canPrint: true
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: __dirname + "/public/index.html",
+            favicon: __dirname + "/public/favicon.ico",
+            inject: 'body',
+            files: {
+                css: [__dirname + "main.css"],
+                js: [__dirname + "main.js"],
+            }
+        })
     ]
 };
 
