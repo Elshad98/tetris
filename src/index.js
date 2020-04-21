89c61c597534443b92f0dc05d71691ee4867c224
Elshad98 <safarov.elshad007@gmail.com>
2020-04-21 23:40:28 +0500

    Create a View class

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import Game from './js/game';
+import View from './js/view';
 
 import 'reset-css';
 import './css/main.css';
 
+const element = document.getElementById('root');
+
 const game = new Game();
+const view = new View(element, 480, 640, 20, 10);
 
 window.game = game;
-
-console.log(game);
+window.view = view;
---
+++ src/js/view.js
@@ -0,0 +1,16 @@
+class View {
+    constructor(element, width, height, rows, columns) {
+        this.element = element;
+        this.width = width;
+        this.height = height;
+        
+        this.canvas = document.createElement('canvas');
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+        this.contenxt = this.canvas.getContext('2d');
+
+        this.element.appendChild(this.canvas);
+    }
+}
+
+export default View;
