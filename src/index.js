9c309c1008f29376901d2e904b5fb2f522bb3667
Elshad98 <safarov.elshad007@gmail.com>
2021-06-03 19:58:09 +0500

    Added adaptive panel with buttons

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import { WIDTH, HEIGHT, COLUMNS, ROWS, } from './js/constants';
 import 'reset-css';
 import './css/main.css';
 
-window.addEventListener('load', function () {
+window.addEventListener('load', function() {
     const rootEl = document.getElementById('root');
     const element = document.querySelector('.game-screen');
+    const gameControls = document.querySelector('.game-controls');
 
     const game = new Game();
     const view = new View(element, WIDTH, HEIGHT, ROWS, COLUMNS);
@@ -22,7 +23,7 @@ window.addEventListener('load', function () {
         rotation: document.querySelector('.rotation-button')
     });
 
-    const transform = (function () {
+    const transform = (function() {
         const trans = ['transform', 'webkitTransform', 'msTransform', 'mozTransform', 'oTransform'];
         const body = document.body;
         return trans.filter((e) => body.style[e] !== undefined)[0];
@@ -33,12 +34,14 @@ window.addEventListener('load', function () {
         const height = document.documentElement.clientHeight;
         const ratio = height / width;
         let scale;
-        rootEl.style.paddingTop = `42px`;
         if (ratio < 1.5) {
+            rootEl.style.paddingTop = `42px`;
+            gameControls.style.marginTop = `25px`;
             scale = height / 960;
         } else {
             scale = width / 640;
             let filling = (height - (960 * scale)) / scale / 3;
+            gameControls.style.marginTop = `${filling + 25}px`;
             rootEl.style.paddingTop = `${Math.floor(filling) + 42}px`;
             rootEl.style.paddingBottom = `${Math.floor(filling)}px`;
             rootEl.style.marginTop = `${Math.floor(-480 - (filling * 1.5))}px`;
